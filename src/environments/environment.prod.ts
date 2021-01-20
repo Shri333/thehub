f85e774938df4c6d41ae3eb12960b8c287a2491c
Shri333 <dshrihan@gmail.com>
2021-01-20 10:00:34 -0500

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiKey: "a12265022fc9edbf3d731a1c553b66ab",
+  weatherApiUrl: "https://api.openweathermap.org/data/2.5/onecall",
+  newsApiKey: "6dd2bb48d5eb469984152ba768b9d049",
+  newsApiUrl: "https://newsapi.org/v2/top-headlines"
 };
