eccbc892f869f4fb1241aad0178d78058039609a
Shri333 <dshrihan@gmail.com>
2021-01-20 16:45:43 -0500

    new gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiKey: "a12265022fc9edbf3d731a1c553b66ab",
-  weatherApiUrl: "https://api.openweathermap.org/data/2.5/onecall",
-  newsApiKey: "4cb3d172-d8f8-4798-a502-b6740750f7f6",
-  newsApiUrl: "https://content.guardianapis.com/search"
-};
